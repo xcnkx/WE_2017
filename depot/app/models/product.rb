6b5725a4af41129e552cb7d91cbcf38d5ead4377
xcnkx <cnk_2806@me.com>
2017-11-11 20:20:50 +0900

    debug

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
-  validates :tittle, :description, :image_url, presence: true
+  validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :tittle, uniqueness:true
   validates:image_url, allow_blank: true, format: {
