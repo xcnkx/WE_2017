e2919ab4c0f3156005de138f0cbc911de7eb4a5f
xcnkx <cnk_2806@me.com>
2017-11-14 02:08:29 +0900

    validates registration

--- registration/app/models/user.rb
+++ registration/app/models/user.rb
@@ -1,8 +1,4 @@
 class User < ApplicationRecord
   validates :name, :affiliation, :contact_adress, :phone_number, :title, :abstraction,  presence: true
   validates :title, uniqueness: true
-  validates :image_url, allow_blank: true, format: {
-    with: %r{\.(gif|jpg|png)\Z}i,
-    message: 'must be a URL for GIF, JPG or PNG image'
-  }
 end
