e27f2c58961ff10eabbd2cde6a73f44e995b182f
xcnkx <cnk_2806@me.com>
2017-11-14 11:49:58 +0900

    finished depot catalog

--- depot/config/routes.rb
+++ depot/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :products
+  get "store/index"
+  root 'store#index', as: 'store'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
